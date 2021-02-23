e1f99da1abea7e50ac66b3ac2a9495a04b175fe8
Jonathan Hornby <jtorn9@gmail.com>
2021-02-23 22:42:48 +1000

    Done Leapyear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,6 +14,17 @@ public class LeapYear {
         }
     }
 
+    public static boolean isLeapYear(int year) {
+
+    	/** Calculates if a given year is a leapyear or not. Returns True or False */
+    	if (year % 400 == 0) {
+    		return true;
+    	} else if (year % 4 == 0 && year % 100 != 0) {
+    		return true;
+    	}
+    	return false;
+    }
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
